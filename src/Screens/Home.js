ad05ac84896ab8747d0b30eabe9df4f2d7290b17
Vinod Patil <61724808+vinodpatildev@users.noreply.github.com>
2023-02-14 15:37:48 +0530

    14 Feb 2023 [Vinod Patil] : Home layout is updated, front logo is added, horizontal line images are added.

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,24 +1,48 @@
 import React from 'react';
-import {View, Text, Image, Button} from 'react-native';
-import {ScrollView, TouchableOpacity} from 'react-native-gesture-handler';
+import {View, Text, Image, StyleSheet, ImageBackground} from 'react-native';
+import {
+  FlatList,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native-gesture-handler';
+import { color, processColor } from 'react-native-reanimated';
 
 const Home = () => {
   return (
     <ScrollView>
-      <View style={{flex: 1, flexDirection: 'column', padding: 8}}>
+      <View style={{flex: 1, flexDirection: 'column', margin: 8}}>
+        <View
+          style={{
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginTop:32
+          }}>
+          <Image source={require('../Asset/Home/flourisense_logo.png')} style={{ height: 250, width: 250 }} />
+        </View>
         <View
           style={{
             flexDirection: 'row',
             alignItems: 'center',
             justifyContent: 'center',
             marginTop: 32,
-            marginBottom: 32,
-            padding: 16,
           }}>
-          <Image source={require('../Asset/Home/flourisense.jpg')} />
+          <Image source={require('../Asset/Home/flourisense.png')} style={{ height: 100, width: 200, tintColor:'#E9282B'  }}/>
         </View>
 
-        <View>
+        <View
+          style={{
+            margin: 8,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <Image
+            resizeMode="contain"
+            source={require('../Asset/Home/hr_line_flower.png')}
+            style={{height: 40}}
+          />
+        </View>
+
+        <View style={{marginTop: 64}}>
           <View style={{marginVertical: 16, alignItems: 'center'}}>
             <Text
               style={{
@@ -42,9 +66,32 @@ const Home = () => {
               the technology expertise to make this happen.
             </Text>
           </View>
+          <View style={{flex: 1}}>
+            <FlatList
+              data={CLIENTS_DATA}
+              renderItem={renderClientsCardView}
+              keyExtractor={item => item.id}
+              numColumns={2}
+              style={styles.container}
+              contentContainerStyle={styles.list}
+            />
+          </View>
         </View>
 
-        <View>
+        <View
+          style={{
+            margin: 8,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <Image
+            resizeMode="contain"
+            source={require('../Asset/Home/hr_line_flower.png')}
+            style={{height: 40}}
+          />
+        </View>
+
+        <View style={{marginTop: 64}}>
           <View style={{marginVertical: 16, alignItems: 'center'}}>
             <Text
               style={{
@@ -64,8 +111,7 @@ const Home = () => {
                 color: '#000000',
                 textAlign: 'center',
               }}
-              numberOfLines={5}
-              >
+              numberOfLines={5}>
               Flourisense designs and builds intelligent, cost-effective and
               intuitive web applications, cloud systems, desktop applications
               and mobile apps that help streamline processes for businesses as
@@ -85,12 +131,27 @@ const Home = () => {
                 padding: 12,
                 borderRadius: 5,
               }}>
-              <Text style={{color: '#fff', fontWeight: 'bold'}}>CALL US TODAY</Text>
+              <Text style={{color: '#fff', fontWeight: 'bold'}}>
+                CALL US TODAY
+              </Text>
             </TouchableOpacity>
           </View>
         </View>
 
-        <View>
+        <View
+          style={{
+            margin: 8,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <Image
+            resizeMode="contain"
+            source={require('../Asset/Home/hr_line_flower.png')}
+            style={{height: 40}}
+          />
+        </View>
+
+        <View style={{marginTop: 64}}>
           <View style={{marginVertical: 16, alignItems: 'center'}}>
             <Text
               style={{
@@ -123,13 +184,84 @@ const Home = () => {
                 padding: 12,
                 borderRadius: 5,
               }}>
-              <Text style={{color: '#fff', fontWeight: 'bold'}}>JOIN US TODAY</Text>
+              <Text style={{color: '#fff', fontWeight: 'bold'}}>
+                JOIN US TODAY
+              </Text>
             </TouchableOpacity>
           </View>
+        </View>
+
+        <View
+          style={{
+            margin: 8,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <Image
+            resizeMode="contain"
+            source={require('../Asset/Home/hr_line_flower.png')}
+            style={{height: 40}}
+          />
         </View>
       </View>
     </ScrollView>
   );
 };
-
 export default Home;
+
+const CLIENTS_DATA = [
+  {
+    id: '1',
+    image: require('../Asset/clients/client_swaraj_associate.png'),
+  },
+  {
+    id: '2',
+    image: require('../Asset/clients/client_axio.png'),
+  },
+  {
+    id: '3',
+    image: require('../Asset/clients/client_bits.png'),
+  },
+  {
+    id: '5',
+    image: require('../Asset/clients/client_fern_stays.png'),
+  },
+];
+const ClientsCardView = ({clientItem}) => (
+  <View style={styles.card_layout}>
+    <ImageBackground
+      source={clientItem.image}
+      resizeMode="contain"
+      style={styles.image}></ImageBackground>
+  </View>
+);
+const renderClientsCardView = ({item}) => {
+  return <ClientsCardView clientItem={item} />;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 8,
+    flexDirection: 'column',
+  },
+  list: {
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+    borderRadius: 10,
+    height: 120,
+    width: 160,
+  },
+
+  card_layout: {
+    height: 120,
+    width: 160,
+    borderRadius: 10,
+    justifyContent: 'flex-start',
+    margin: 3,
+  },
+});
