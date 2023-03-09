a53b31ae4b5b074588daa94a101a668f0f00e22a
Vinod Patil <61724808+vinodpatildev@users.noreply.github.com>
2023-03-09 14:16:07 +0530

    [09 03 2023 : Vinod Patil] : NotFound404Page.js created.

---
+++ src/Screens/Errors/NotFound404Page.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {View, Text, Image, StyleSheet, ImageBackground} from 'react-native';
+import {
+  FlatList,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native-gesture-handler';
+
+const NotFound404Page = () => {
+  return (
+    <View style={{flex: 1, flexDirection: 'column', padding: 16, alignItems: 'center', justifyContent: 'center',}}>
+      <View
+        style={{
+          alignItems: 'center',
+          justifyContent: 'center',
+          marginTop: 0,
+          height: 270, 
+          width: 270,
+        }}>
+        <Image
+          source={require('../../Asset/errors/error_404.png')}
+          resizeMode="contain"
+          style={{height: 250, width: 250}}
+        />
+      </View>
+      <View style={{marginTop: 64}}>
+        <Text
+          style={{
+            fontSize: 18,
+            fontWeight: 'bold',
+            color: '#000000',
+            textAlign: 'center',
+          }}>
+          Not Found (404)
+        </Text>
+      </View>
+      <View style={{marginVertical: 8, alignItems: 'center'}}>
+        <Text
+          style={{
+            fontSize: 14,
+            fontWeight: 'bold',
+            color: '#6E798C',
+            textAlign: 'center',
+          }}>
+          Sorry, the page you are looking for do not exist. Take me to home
+          screen.
+        </Text>
+      </View>
+    </View>
+  );
+};
+export default NotFound404Page;
+
